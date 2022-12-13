a100786d3a14a35b7b3173383367f08bb185bf68
Jorsary <edikus29@gmail.com>
2022-12-13 21:17:35 +0300

    feat: сделать блок voice

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
-const upButton = document.querySelector(".table__button_type_up");
-const downButton = document.querySelector(".table__button_type_down");
+const buttonToUp = document.querySelector(".table__button_type_up");
+const buttonToDown = document.querySelector(".table__button_type_down");
 const tableTop = document.querySelector("#tableTop");
+const frameImg = document.querySelector(".voice__frame-img");
+const hole = document.querySelector(".voice__hole");
 
-const handlerButtonUp = () => {
+buttonToUp.addEventListener("click", () => {
   tableTop.classList.add("table__img_type_elevate");
-};
+});
 
-const handlerButtonDown = () => {
+buttonToDown.addEventListener("click", () => {
   tableTop.classList.remove("table__img_type_elevate");
-};
+});
 
-upButton.addEventListener("click", handlerButtonUp);
+function setHoleClosed() {
+  hole.classList.remove("voice__hole_type_opened");
+}
 
-downButton.addEventListener("click", handlerButtonDown);
+function showFrame() {
+  frameImg.classList.remove("voice__frame-img_type_hide");
+}
+
+function frameAnimation() {
+  const currentPosition =
+    window.pageYOffset + document.documentElement.clientHeight;
+  const framePosition =
+    frameImg.getBoundingClientRect().bottom + window.pageYOffset;
+
+  if (currentPosition >= framePosition) {
+    hole.classList.remove("voice__hole_type_hide");
+    setTimeout(showFrame, 500);
+    setTimeout(setHoleClosed, 3000);
+  }
+}
+
+document.addEventListener("scroll", frameAnimation);
